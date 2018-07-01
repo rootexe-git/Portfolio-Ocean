11b59ca89cc8c7bb8a6790b50c7d8ba285f55ffe
Yash Zolmajdi <yash.zolmajdi@plutora.com>
2018-07-01 23:16:33 +1000

    uplift

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     try {
-        TagCanvas.textColour = '#1e88e5';
+        TagCanvas.textColour = '#ffffff';
         TagCanvas.outlineMethod = 'none';
         TagCanvas.textFont = 'Roboto, sans-serif';
         TagCanvas.Start('skillCanvas');
@@ -8,3 +8,14 @@ window.onload = function () {
         document.getElementById('skillContainer').style.display = 'none';
     }
 };
+
+window.AOS.init();
+
+document.getElementById("sendEmail").addEventListener("click", function(e) {
+    e.target.style.display = "none";
+    document.getElementById("confirmEmail").style.display = "block";
+    setTimeout(function(){ 
+        e.target.style.display = "block";
+        document.getElementById("confirmEmail").style.display = "none";
+    }, 5000);
+});
--- js/nav.js
+++ js/nav.js
@@ -4,7 +4,7 @@
     var sticky = navbar.offsetTop;
 
     window.onscroll = function () {
-        if ((window.pageYOffset+60) >= sticky) {
+        if (window.pageYOffset - 60 >= sticky) {
             navbar.classList.add("sticky")
         } else {
             navbar.classList.remove("sticky");
